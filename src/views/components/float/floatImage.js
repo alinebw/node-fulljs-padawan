dc78246489c7cbf0490cfcb9cc51ecc15258d0e7
rodrigo fleck <alinebranco8@gmail.com>
2021-06-02 06:25:10 -0300

    structuring elements

---
+++ src/views/components/float/floatImage.js
@@ -0,0 +1,9 @@
+import YodaBaby from '../../../assets/images/elements/baby-yoda.png'
+
+let floatImage = ` 
+<div class="mais-left">
+<img src="${YodaBaby}" alt="Baby Yoda" width="120px" height="auto">
+</div>
+`
+
+export default floatImage;
